234fd139a8d22f2bf195407833bfbac1e81da8f6
Wang Stella <1819115430@qq.com>
2025-09-24 23:37:12 +0800

    add script folder
    add script folder with shell files

---
+++ hw1/script/folder.sh
@@ -0,0 +1,6 @@
+python \
+    hw1/watermark.py \
+    hw1/images \
+    --font-size 35 \
+    --color red \
+    --position top-left
---
+++ hw1/script/single_image.sh
@@ -0,0 +1,6 @@
+python \
+    hw1/watermark.py \
+    hw1/image/000000000.jpg \
+    --font-size 35 \
+    --color "#ffffff" \
+    --position bottom-right
